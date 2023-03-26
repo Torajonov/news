5ead7d27c881d25bcf3a65308f44f7cf1d01923a
Torajonov <torajonov3112@gmail.com>
2023-03-26 14:30:47 +0500

    updatee

--- mynews/context_pro.py
+++ mynews/context_pro.py
@@ -2,16 +2,16 @@ from .models import *
 
 def news(request):
 		categories = Category.objects.all()
-		home = News.objects.filter(home=True).order_by('-id')[:4]
-		last_4 = News.objects.filter(home=False,slider=False).order_by('-id')[:4]
-		footer = News.objects.filter(home=False,slider=False).order_by('?')[:3]
+		home = News.objects.filter(home=True).order_by('-id')[:6]
+		last_4 = News.objects.filter(home=False,slider=False).order_by('-id')
+		footer = News.objects.filter(home=False,slider=False).order_by('?')[:1]
 		try:
 			mini = News.objects.filter(home=False,slider=False).order_by('-id')[4:8]
 			mini_2 = News.objects.filter(home=False,slider=False).order_by('-id')[8:12]
 			slider = News.objects.filter(slider=True).order_by('-id')[:6]
-			popular = News.objects.filter(popular=True).order_by('-id')[:3]
-			reviews = News.objects.all().order_by('-views')[:3]
-			most_commented = News.objects.all().order_by('-comment')[:3]
+			popular = News.objects.filter(popular=True).order_by('-id')[:2]
+			reviews = News.objects.all().order_by('-views')[:2]
+			most_commented = News.objects.all().order_by('-comment')[:2]
 			print(most_commented)
 		except:
 			mini_2 = News.objects.all().order_by('?')[:4]
